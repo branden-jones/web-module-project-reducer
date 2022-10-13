954edad510ade829accfd2b255d2426b283b683d
Branden Jones <branden.wayne.jones@gmail.com>
2022-10-13 14:08:32 -0400

    subtract and multiply are connected

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,17 @@ import TotalDisplay from "./TotalDisplay";
 import CalcButton from "./CalcButton";
 
 import reducer, { initialState } from "../reducers";
-import { applyNumber } from "../actions";
+import { applyNumber, changeOperation } from "../actions";
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const handleNumberClick = (number) => {
     dispatch(applyNumber(number));
+  };
+
+  const handleChangeOperation = (operation) => {
+    dispatch(changeOperation(operation));
   };
 
   return (
@@ -62,9 +66,9 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={"+"} />
-              <CalcButton value={"*"} />
-              <CalcButton value={"-"} />
+              <CalcButton value={"+"} onClick={() => handleChangeOperation("+")} />
+              <CalcButton value={"*"} onClick={() => handleChangeOperation("*")} />
+              <CalcButton value={"-"} onClick={() => handleChangeOperation("-")} />
             </div>
 
             <div className="row ce_button">
